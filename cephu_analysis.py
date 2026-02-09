cb160a8b1bc19235a51129f10c8cb4626566326c
Sofie Coudeville <sofie@analytiq.be>
2026-02-09 18:46:27 +0100

    Make HTML export responsive
    Changed HTML export to be responsive.

--- cephu_analysis.py
+++ cephu_analysis.py
@@ -95,7 +95,7 @@ def generate_clean_basis_chart_html():
 
     # --- EXPORT NAAR HTML ---
     # Gebruik include_plotlyjs='cdn' om het bestand klein te houden
-    fig.write_html("index.html", include_plotlyjs='cdn', full_html=True)
+    fig.write_html("index.html", include_plotlyjs='cdn', full_html=False, config={'responsive': True})
     
     # Voeg meta-refresh toe (900 seconden = 15 minuten)
     with open("index.html", "r+") as f:
